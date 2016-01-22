489cd736cf5a011e281a06cf7b34932687f83cf6
bkonkle <brandon@konkle.us>
2016-01-22 01:09:09 -0700

    Made progress on the compiler

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,10 @@
 import findup from 'findup-sync'
 import minimist from 'minimist'
 
+const DEFAULTS = {
+  source: 'src/**/*.js?(x)',
+}
+
 export default function getConfig(argv) {
   const config = findup('.shipyardrc')
 
@@ -8,7 +12,7 @@ export default function getConfig(argv) {
   const options = minimist(args, {
     alias: {verbose: 'v'},
     boolean: ['verbose', 'quiet', 'silent'],
-    default: config,
+    default: {...DEFAULTS, ...config},
   })
 
   return {...options}
