ca04ca4904a15de4f4d8cf8a98cc1a2c35943812
bkonkle <brandon@konkle.us>
2016-01-26 00:50:10 -0700

    Add the linter reducer

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,8 +1,9 @@
 import {combineReducers} from 'redux'
 import foreman from './foreman'
+import linter from 'workers/linter/state'
 import transpiler from 'workers/transpiler/state'
 import workers from 'workers/state'
 
 export function getReducer() {
-  return combineReducers({foreman, transpiler, workers})
+  return combineReducers({foreman, linter, transpiler, workers})
 }
